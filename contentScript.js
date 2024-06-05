8b901901d0684a9cf0a1946f765a473132685da4
SamsulAripin77 <arifins192@gmail.com>
2024-06-05 13:45:49 +0700

    initial commit

---
+++ contentScript.js
@@ -0,0 +1,28 @@
+function speeder(){
+    let videos = document.querySelectorAll('video')
+    let add = document.querySelector('.ytp-ad-overlay-container.ytp-rounded-overlay-ad');
+    let skip = document.querySelector('button.ytp-skip-ad-button');
+    let unskip = document.querySelector('.ytp-ad-text.ytp-ad-preview-text')
+        if(add){
+	  add.style.display = 'none';	
+	}
+        if(skip || unskip){
+	  videos.forEach(function(vid){
+	    	vid.muted = true;
+	    	vid.playbackRate = 10;	
+			console.log('video muted........')
+	     })
+        
+            console.log('Feel Free Speeding: ........😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁')
+        }
+	if( skip ) {
+          skip.click();
+          console.log('Feel Free Skiping: .......😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁') 	
+	}
+}
+
+setInterval(() => {
+    setTimeout(() => {
+        speeder();
+    }, 500)
+}, 500);
